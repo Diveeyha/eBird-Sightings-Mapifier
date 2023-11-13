e4f48987e31e8773ae15289ab4b6ba46d9e4183e
Diveeyha <67159522+Diveeyha@users.noreply.github.com>
2023-11-13 13:00:32 -0500

    Update eBird-Sightings-Mapifier.py

--- eBird-Sightings-Mapifier.py
+++ eBird-Sightings-Mapifier.py
@@ -42,7 +42,7 @@ def main():
         df = df.loc[df['Common_Name'] == st.session_state.user_choice]
 
         complete_map = map_call(df)
-        st_data = st_folium(complete_map)  # width=700, height=500
+        st_data = st_folium(complete_map, width = 725)  # width=700, height=500
         make_map_responsive = """
          <style>
          [title~="st.iframe"] {width: 100%}
